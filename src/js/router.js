31035f786455d4aa183c5730c4638be1217eefb0
illusbyhoppity <honiism@gmail.com>
2025-05-27 14:30:53 +0700

    changed router for github pages

--- src/js/router.js
+++ src/js/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Landing from '../Landing.vue';
 import Home from '../Home.vue'
 
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: routes
 });
 
